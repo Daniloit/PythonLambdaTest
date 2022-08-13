4e3f318016cfe95357ec5d7b4099f19ea0324db8
Danilo Jankov <danilo96it@gmail.com>
2022-08-13 06:22:14 -0700

    add docker file, change lambda code

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,11 @@ import json
 
 
 def lambda_handler(event, context):
+    body=json.loads(event["body"])
+    broj1=int(body["broj1"])
+    broj2=int(body["broj2"])    
+    
     return {
         "status": "OK",
-        "event": json.dumps(event)
+        "event": json.dumps(broj1+broj2)
     }
